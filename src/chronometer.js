2445503d3b4743354840848d40a84c742459fa33
prclima <pedro@futuranet.com.br>
2022-10-30 15:01:40 -0300

    Lab feito

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,39 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+   this.currentTime = 0;
+   this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => this.currentTime ++,1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+   return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return Math.floor(this.currentTime % 60)
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+   if (value.toString().length !== 1) {
+    return value.toString()
+   } else if (value === 0) {
+    return '00'
+   } else {
+    return '0' + value.toString()
+   }
+   
   }
 
   stop() {
-    // ... your code goes here
+    
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
